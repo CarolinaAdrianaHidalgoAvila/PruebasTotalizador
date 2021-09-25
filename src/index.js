932418a299da50918376e67736efe04164d8ccf0
Carolina Adriana Hidalgo Avila <carolina.hidalgo@ucb.edu.bo>
2021-09-25 09:57:01 -0400

    prueba:cantidad_precio_Ingresada

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const form = document.querySelector("#totalizer-form");
 const quantity = document.querySelector("#quantity-input");
 
 const quantity_output = document.querySelector("#show-quantity");
+const price_output = document.querySelector("#show-price");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
